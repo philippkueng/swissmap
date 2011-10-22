439899ae2fceb6e1065931e86c6380e17d191de1
Philipp Küng <i@philippkueng.ch>
2011-10-22 15:43:13 +0200

    added coat of arms from wikipedia with language translations for cantons

---
+++ data/cantons.js
@@ -0,0 +1,210 @@
+window.cantons = {
+  ZH: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Z%C3%BCrich_matt.svg',
+    enlish: 'Zurich',
+    german: 'Zürich',
+    french: 'Zurich',
+    italian: 'Zurigo',
+    romansh: 'Turitg'
+  },
+  BE: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Bern_matt.svg',
+    enlish: 'Bern',
+    german: 'Bern',
+    french: 'Berne',
+    italian: 'Berna',
+    romansh: 'Berna'
+  },
+  LU: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Luzern_matt.svg',
+    enlish: 'Lucerne',
+    german: 'Luzern',
+    french: 'Lucerne',
+    italian: 'Lucerna',
+    romansh: 'Lucerna'
+  },
+  UR: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Uri_matt.svg',
+    enlish: 'Uri',
+    german: 'Uri',
+    french: 'Uri',
+    italian: 'Uri',
+    romansh: 'Uri'
+  },
+  SZ: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_des_Kantons_Schwyz.svg',
+    enlish: 'Schwyz',
+    german: 'Schwyz',
+    french: 'Schwyz (or Schwytz)',
+    italian: 'Svitto',
+    romansh: 'Sviz'
+  },
+  OW: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Obwalden_matt.svg',
+    enlish: 'Obwalden',
+    german: 'Obwalden',
+    french: 'Obwald',
+    italian: 'Obvaldo',
+    romansh: 'Sursilvania'
+  },
+  NW: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Nidwalden_matt.svg',
+    enlish: 'Nidwalden',
+    german: 'Nidwalden',
+    french: 'Nidwald',
+    italian: 'Nidvaldo',
+    romansh: 'Sutsilvania'
+  },
+  GL: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Glarus_matt.svg',
+    enlish: 'Glarus',
+    german: 'Glarus',
+    french: 'Glaris',
+    italian: 'Glarona',
+    romansh: 'Glaruna'
+  },
+  ZG: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Zug_matt.svg',
+    enlish: 'Zug',
+    german: 'Zug',
+    french: 'Zoug',
+    italian: 'Zugo',
+    romansh: 'Zug'
+  },
+  FR: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Freiburg_matt.svg',
+    enlish: 'Fribourg',
+    german: 'Freiburg',
+    french: 'Fribourg',
+    italian: 'Friborgo',
+    romansh: 'Friburg'
+  },
+  SO: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Solothurn_matt.svg',
+    enlish: 'Solothurn',
+    german: 'Solothurn',
+    french: 'Soleure',
+    italian: 'Soletta',
+    romansh: 'Soloturn'
+  },
+  BS: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Basel-Stadt_matt.svg',
+    enlish: 'Basel-City or Basle-City',
+    german: 'Basel-Stadt',
+    french: 'Bâle-Ville',
+    italian: 'Basilea-Città',
+    romansh: 'Basilea-Citad'
+  },
+  BL: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Basel-Landschaft_matt.svg',
+    enlish: 'Basel-Country, Basle-Country, or Basel-Land',
+    german: 'Basel-Landschaft',
+    french: 'Bâle-Campagne',
+    italian: 'Basilea-Campagna',
+    romansh: 'Basilea-Champagna'
+  },
+  SH: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Schaffhausen_matt.svg',
+    enlish: 'Schaffhausen (Schaffhouse)',
+    german: 'Schaffhausen',
+    french: 'Schaffhouse',
+    italian: 'Sciaffusa',
+    romansh: 'Schaffusa'
+  },
+  AR: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Appenzell_Ausserrhoden_matt.svg',
+    enlish: 'Appenzell Ausserrhoden (Appenzell Outer-Rhodes)',
+    german: 'Appenzell Ausserrhoden',
+    french: 'Appenzell Rhodes-Extérieures',
+    italian: 'Appenzello Esterno',
+    romansh: 'Appenzell dador'
+  },
+  AI: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Appenzell_Innerrhoden_matt.svg',
+    enlish: 'Appenzell Innerrhoden (Appenzell Inner-Rhodes)',
+    german: 'Appenzell Innerrhoden',
+    french: 'Appenzell Rhodes-Intérieures',
+    italian: 'Appenzello Interno',
+    romansh: 'Appenzell dadens'
+  },
+  SG: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_St._Gallen_matt.svg',
+    enlish: 'St. Gallen (St. Gall)',
+    german: 'St. Gallen',
+    french: 'Saint-Gall',
+    italian: 'San Gallo',
+    romansh: 'Son Gagl'
+  },
+  GR: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Graub%C3%BCnden_matt.svg',
+    enlish: 'Graubünden (Grisons)',
+    german: 'Graubünden',
+    french: 'Grisons',
+    italian: 'Grigioni',
+    romansh: 'Grischun'
+  },
+  AG: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Aargau_matt.svg',
+    enlish: 'Aargau (rare: Argovia)',
+    german: 'Aargau',
+    french: 'Argovie',
+    italian: 'Argovia',
+    romansh: 'Argovia'
+  },
+  TG: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Thurgau_matt.svg',
+    enlish: 'Thurgau (Thurgovia)',
+    german: 'Thurgau',
+    french: 'Thurgovie',
+    italian: 'Turgovia',
+    romansh: 'Turgovia'
+  },
+  TI: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Tessin_matt.svg',
+    enlish: 'Ticino',
+    german: 'Tessin',
+    french: 'Tessin',
+    italian: 'Ticino',
+    romansh: 'Tessin'
+  },
+  VD: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Waadt_matt.svg',
+    enlish: 'Vaud',
+    german: 'Waadt',
+    french: 'Vaud',
+    italian: 'Vaud',
+    romansh: 'Vad'
+  },
+  VS: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Wallis_matt.svg',
+    enlish: 'Valais',
+    german: 'Wallis',
+    french: 'Valais',
+    italian: 'Vallese',
+    romansh: 'Vallais'
+  },
+  NE: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Neuenburg_matt.svg',
+    enlish: 'Neuchâtel',
+    german: 'Neuenburg',
+    french: 'Neuchâtel',
+    italian: 'Neuchâtel',
+    romansh: 'Neuchâtel'
+  },
+  GE: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Genf_matt.svg',
+    enlish: 'Geneva',
+    german: 'Genf',
+    french: 'Genève',
+    italian: 'Ginevra',
+    romansh: 'Genevra'
+  },
+  JU: {
+    coat_of_arms_url: 'http://en.wikipedia.org/wiki/File:Wappen_Jura_matt.svg',
+    enlish: 'Jura',
+    german: 'Jura',
+    french: 'Jura',
+    italian: 'Giura',
+    romansh: 'Giura'
+  }
+};
